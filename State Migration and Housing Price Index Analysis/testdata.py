555d71c78e3b8111164a5dabcf759076d2a15ff0
kellyrhoden <163667589+kellyrhoden@users.noreply.github.com>
2024-03-18 01:00:22 -0400

    Update and rename testdata.py to  State Migration and Housing Price Index Analysis/testdata.py

--- State Migration and Housing Price Index Analysis/testdata.py
+++ State Migration and Housing Price Index Analysis/testdata.py
@@ -1,7 +1,5 @@
-# Created by Kelly Rhoden
 # Calculates the pearson coefficient for northern migration and SC HPI
 # Then calculates the pearson coefficient for total migrations from SC and total migrations to SC
-# Created Nov 24 2023
 
 # Imports
 import pandas as pd
